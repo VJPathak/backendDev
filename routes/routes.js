3461ca6ffb7706d79cdefaffc4b31c94e0658806
Vashishth Pathak <94379255+VJPathak@users.noreply.github.com>
2024-03-13 15:50:51 +0530

    Add files via upload

---
+++ routes/routes.js
@@ -0,0 +1,32 @@
+const controller = require('../controllers/controllerAuth');
+
+let postSignup = controller.postSignup;
+let getLogin = controller.getLogin;
+let getBannerOffers = controller.getBannerOffers;
+let getCategories = controller.getCategories;
+let getItemList = controller.getItemList;
+
+const express = require("express");
+let router = express.Router();
+
+router
+  .route("/signup")
+  .get(postSignup);
+
+router
+  .route("/login")
+  .get(getLogin);
+
+router
+  .route("/bannerOffers")
+  .get(getBannerOffers);
+
+router
+  .route("/categories")
+  .get(getCategories);
+
+router
+  .route("/itemList")
+  .get(getItemList);
+
+  module.exports = router;
