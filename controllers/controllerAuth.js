8efe2ac2d32945505abaa72871f2709771633dca
Vashishth Pathak <94379255+VJPathak@users.noreply.github.com>
2024-04-05 16:21:08 +0530

    Update controllerAuth.js

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -1897,8 +1897,7 @@ const postDeleteItem = (req, res) => {
 
     //await db.collection("Vendor's List").doc(vid).collection(category).doc(subcat).set({[itemid]: {"outOfStock":true}}, { merge: true });
 
-    await db.collection("Vendor's List").doc(vid).collection(category).doc(subcat).set({[`${itemid}`]: {"outOfStock":true}}, { merge: true });
-
+    await db.collection("Vendor's List").doc(vid).collection(category).doc(`${subcat}`).set({[`${itemid}`]: {"outOfStock":true}}, { merge: true });
     
     //DONE
     //await db.collection("Vendor's List").doc(vid).collection("Catalogue").doc(itemid).update({"outOfStock":true});
