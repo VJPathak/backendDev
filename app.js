5ead9917cf1d243cbef3325437adf535eb11ff83
Vashishth Pathak <94379255+VJPathak@users.noreply.github.com>
2024-05-05 00:06:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.set('view engine','ejs');
 app.listen(3000, function() { 
   console.log('Server running on port 3000'); 
   console.log("Initiating Our Project :) ");
+  console.log(${{ secrets.SEC }};)
 });
 // }).setTimeout(100000000);
 
